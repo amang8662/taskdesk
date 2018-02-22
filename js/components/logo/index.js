91e01dbef9bf0cbbc864e3138f70c2588f7e8a11
Akhil <chaturvediAkhil@outlook.com>
2018-02-22 22:09:50 +0530

    logo created and set

--- js/components/logo/index.js
+++ js/components/logo/index.js
@@ -10,7 +10,7 @@ export default class Logo extends Component<{}> {
 	render(){
 		return(
 			<View style={styles.container}>
-				<Image  style={{width:40, height: 70}}
+				<Image  style={{width:120, height: 70}}
           			source={require('../../img/logo.png')}/>
           		<Text style={styles.logoText}>Welcome to My app.</Text>	
   			</View>
