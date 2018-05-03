f494f19bc3ab87b5dbe55e17d0fdfbde4b85f13e
Aman Gupta <amang8662@outlook.com>
2018-05-03 17:59:33 +0530

    changes in validate module to check for array

--- js/modules/validate.js
+++ js/modules/validate.js
@@ -12,7 +12,8 @@ var regex = '';
 	regexExp: '',
 	integer: true,
 	min: 0,
-	max: 0
+	max: 0,
+	type: 'string' 'array'
 } */
 module.exports = validate;
 
@@ -22,56 +23,65 @@ function validate(name,value, options = {}) {
 		isError: false,
 		errortype: '',
 		message: ''
-	};
+		};
+	if(options.type && options.type == 'array') {
 
-	if(response.isError == false && options.required) {
-		if(value == '') {
-			response.isError = true;
-			response.errortype = 'required';
-			response.message = ucwords(name) + " cannot be empty";
+		if(response.isError == false && options.required) {
+			if(value.length == 0) {
+				response.isError = true;
+				response.errortype = 'required';
+				response.message = ucwords(name) + " cannot be empty";
+			}
+		}
+	} else {
+		if(response.isError == false && options.required) {
+			if(value == '') {
+				response.isError = true;
+				response.errortype = 'required';
+				response.message = ucwords(name) + " cannot be empty";
+			}
+		}
+
+		if(response.isError == false && options.regex) {
+
+			if(typeof options.regexExp !== "undefined" && options.regexExp !== '') {
+				regex = new RegExp(options.regexExp);
+			} else {
+				regex = getRegex(name);
+			}
+
+			if(!regex.test(value)) {
+
+				response.isError = true;
+				response.errortype = 'regex';
+				response.message = (typeof options.regexMsg !== "undefined") ? options.regexMsg : ucwords(name) + " is not valid";
+			}
+		} 
+
+		if(response.isError == false && options.integer) {
+			if(!Number.isInteger(value)) {
+				response.isError = true;
+				response.errortype = 'integer';
+				response.message = ucwords(name) + "is not an Integer";
+			}
+		} 
+
+		if(response.isError == false && options.min && Number.isInteger(options.min) && options.min >= 0) {
+			if(value.length < options.min) {
+				response.isError = true;
+				response.errortype = 'min';
+				response.message = "Minimum length of " + ucwords(name) + " must be " + options.min;
+			}
+		} 
+
+		if(response.isError == false && options.max && Number.isInteger(options.max) && options.max <= 10000) {
+			if(value.length > options.max) {
+				response.isError = true;
+				response.errortype = 'max';
+				response.message = "Maximum length " + ucwords(name) + " must be " + options.max;
+			}
 		}
 	}
-
-	if(response.isError == false && options.regex) {
-
-		if(typeof options.regexExp !== "undefined" && options.regexExp !== '') {
-			regex = new RegExp(options.regexExp);
-		} else {
-			regex = getRegex(name);
-		}
-
-		if(!regex.test(value)) {
-
-			response.isError = true;
-			response.errortype = 'regex';
-			response.message = (typeof options.regexMsg !== "undefined") ? options.regexMsg : ucwords(name) + " is not valid";
-		}
-	} 
-
-	if(response.isError == false && options.integer) {
-		if(!Number.isInteger(value)) {
-			response.isError = true;
-			response.errortype = 'integer';
-			response.message = ucwords(name) + "is not an Integer";
-		}
-	} 
-
-	if(response.isError == false && options.min && Number.isInteger(options.min) && options.min >= 0) {
-		if(value.length < options.min) {
-			response.isError = true;
-			response.errortype = 'min';
-			response.message = "Minimum length of " + ucwords(name) + " must be " + options.min;
-		}
-	} 
-
-	if(response.isError == false && options.max && Number.isInteger(options.max) && options.max <= 10000) {
-		if(value.length > options.max) {
-			response.isError = true;
-			response.errortype = 'max';
-			response.message = "Maximum length " + ucwords(name) + " must be " + options.max;
-		}
-	}
-
 	return response;
 }
 
