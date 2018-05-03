3d51a1e47760b46abbbfb8969e9de92c265fafa1
Aman Gupta <amang8662@outlook.com>
2018-05-03 18:19:20 +0530

    schema for task

---
+++ backend/models/Task.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from 'mongoose';
+var uniqueValidator = require('mongoose-unique-validator');
+
+// Define task schema
+var taskSchema = new Schema({
+  task_creater: { 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  task_taker: { 
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  rewardscore: {
+    type: Number,
+  },
+  status: {
+    type: Number,
+    default: 0,
+  },
+  skills: [
+    { 
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Skill'
+    }
+  ],
+});
+taskSchema.plugin(uniqueValidator);
+
+// Export Mongoose model
+export default mongoose.model('Task', taskSchema);
