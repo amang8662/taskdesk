7061b8308c35fc620616af004f49037504aa1e1a
Akhil <ChaturvediAkhil@outlook.com>
2018-06-15 09:40:57 +0530

    bottom nav removed

--- js/Routes.js
+++ js/Routes.js
@@ -36,26 +36,26 @@ export default class Routes extends Component<{}> {
 				    </Stack>
 				    <Scene key="authenticated" hideNavBar type="reset">
     			    	<Scene key="drawer" drawer contentComponent={SideMenu} drawerPosition="left" drawerWidth={240}  initial hideDrawerButton={true}>
-    			    		<Scene key="tabs" tabs tabBarPosition="bottom" showIcon={true}>
-	    			    		<Stack key="dashboard" tabBarIcon={iconDashBoard} title="DashBoard" initial>
-	    		    				<Scene key="home" component={Home} title="Home" initial hideNavBar/>
-	    		    				<Scene key="taskinfo" component={TaskInfo} title="TaskInfo"/>
-	    			    		</Stack>
-	    			    		
-	    			    		<Scene key="addtask" component={AddTask} tabBarIcon={iconAddCircle} title="Add Task"/>
-	    		    			
-	    		    			<Stack key="profile" title="Profile" tabBarIcon={iconPerson}>
-	    		    				<Scene key="profilehome" component={ProfileHome} initial hideNavBar/>
-	    		    				<Scene key="editprofile" component={EditProfile} title="Edit Profile"/>
-	    		    			</Stack>
-	    			    	</Scene>
+    			    		
+    			    		<Stack key="dashboard" tabBarIcon={iconDashBoard} title="DashBoard" initial>
+    		    				<Scene key="home" component={Home} title="Home" initial hideNavBar/>
+    		    				<Scene key="taskinfo" component={TaskInfo} title="TaskInfo" hideNavBar/>
+    			    		</Stack>
+    			    		
+    		    			
+    		    			<Stack key="profile" title="Profile" tabBarIcon={iconPerson}>
+    		    				<Scene key="profilehome" component={ProfileHome} initial hideNavBar/>
+    		    				<Scene key="editprofile" component={EditProfile} hideNavBar title="Edit Profile"/>
+    		    			</Stack>
+	    			    	
 
 	    			    	<Stack key="task" >
 	    			    		<Scene key="generatedtasks" component={GeneratedTasks} title="Generated Tasks" initial hideNavBar/>
-	    			    		<Scene key="edittask" component={EditTask} title="Edit Task"/>
+	    			    		<Scene key="addtask" component={AddTask} tabBarIcon={iconAddCircle} hideNavBar title="Add Task"/>
+	    			    		<Scene key="edittask" component={EditTask} hideNavBar title="Edit Task"/>
     			    		</Stack>
 
-    			    		<Scene key="yourtasks" component={YourTasks} title="Your Tasks"/>
+    			    		<Scene key="yourtasks" component={YourTasks} hideNavBar title="Your Tasks"/>
     			    		<Scene key="aboutus" component={AboutUs} title="About Us"/>
     			    		<Scene key="addskill" component={AddSkill} title="Add Skill"/>
     			    	</Scene>
